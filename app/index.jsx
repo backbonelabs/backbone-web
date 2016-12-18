864d1df9e866700e146dbbf32cb4b45bc5155a89
Miguel Correa <miguelc1221@gmail.com>
2016-12-17 23:08:04 -0800

    add react-hot-loader

--- app/index.jsx
+++ app/index.jsx
@@ -1,10 +1,25 @@
 import React from 'react';
-import { render } from 'react-dom';
+import ReactDOM from 'react-dom';
 import './global.scss';
 
 import App from './components/App';
+import { AppContainer } from 'react-hot-loader'; // eslint-disable-line
 
-render(
-  <App />,
-  document.getElementById('root'), // eslint-disable-line
-);
+const rootEl = document.getElementById('root'); // eslint-disable-line
+const renderApp = (Component = App) => {
+  ReactDOM.render(
+    <AppContainer>
+      <Component />
+    </AppContainer>,
+    rootEl,
+  );
+};
+
+if (module.hot) {
+  module.hot.accept('./components/App', () => {
+    const NextApp = require('./components/App').default // eslint-disable-line
+    renderApp(NextApp);
+  });
+}
+
+renderApp();
--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -5,6 +5,7 @@ const autoprefixer = require('autoprefixer');
 module.exports = {
   devtool: 'cheap-module-eval-source-map',
   entry: [
+    'react-hot-loader/patch',
     'webpack-hot-middleware/client?reload=true',
     './app/index',
   ],
