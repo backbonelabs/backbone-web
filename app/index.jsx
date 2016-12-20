c22026950b5a2dfefd36473eb50e972d133f02e4
Miguel Correa <miguelc1221@gmail.com>
2016-12-20 12:34:46 -0800

    Add material-ui

--- app/index.jsx
+++ app/index.jsx
@@ -2,16 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { browserHistory } from 'react-router';
 import { AppContainer } from 'react-hot-loader';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import routes from './routes';
 
 import './global.scss';
 
+// Needed for onTouchTap
+// http://stackoverflow.com/a/34015469/988941
+injectTapEventPlugin();
+
 const rootEl = document.getElementById('root');
 const renderApp = (Component = routes) => {
   ReactDOM.render(
-    <AppContainer>
-      <Component history={browserHistory} />
-    </AppContainer>,
+    <MuiThemeProvider>
+      <AppContainer>
+        <Component history={browserHistory} />
+      </AppContainer>
+    </MuiThemeProvider>,
     rootEl,
   );
 };
