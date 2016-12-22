16540d6da269c8db62a36ec2ec48b903075b08a5
Miguel Correa <miguelc1221@gmail.com>
2016-12-21 22:06:49 -0800

    add onChange prop

--- app/components/Login/Login.jsx
+++ app/components/Login/Login.jsx
@@ -1,14 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Form from '../common/Form/Form';
 
 import './Login.scss';
 
-const Login = () => (
-  <div className="login-container">
-    <Form
-      login
-    />
-  </div>
-);
+class Login extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      email: '',
+      password: '',
+    };
+
+    this.handleOnChange = this.handleOnChange.bind(this);
+  }
+
+  handleOnChange(evt) {
+    this.setState({ [evt.target.name]: evt.target.value });
+  }
+
+  render() {
+    return (
+      <div className="login-container">
+        <Form
+          login
+          onChange={this.handleOnChange}
+        />
+      </div>
+    );
+  }
+}
 
 export default Login;
--- app/components/Login/Signup.jsx
+++ app/components/Login/Signup.jsx
@@ -1,12 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Form from '../common/Form/Form';
 
 import './Login.scss';
 
-const Signup = () => (
-  <div className="login-container">
-    <Form signup />
-  </div>
-);
+class Signup extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      email: '',
+      password: '',
+      confirmPassword: '',
+    };
+
+    this.handleOnChange = this.handleOnChange.bind(this);
+  }
+
+  handleOnChange(evt) {
+    this.setState({ [evt.target.name]: evt.target.value });
+  }
+
+  render() {
+    return (
+      <div className="login-container">
+        <Form
+          signup
+          onChange={this.handleOnChange}
+        />
+      </div>
+    );
+  }
+}
 
 export default Signup;
