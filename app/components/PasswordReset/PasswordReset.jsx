c8411524f06cbecde4272c16334ad0dfcba37402
Kevin Huang <huang.kev@gmail.com>
2017-01-11 16:35:17 -0800

    Use same styles as login and signup

--- app/components/PasswordReset/PasswordReset.jsx
+++ app/components/PasswordReset/PasswordReset.jsx
@@ -4,8 +4,9 @@ import TextField from 'material-ui/TextField';
 import RaisedButton from 'material-ui/RaisedButton';
 import { red500 } from 'material-ui/styles/colors';
 import Form from '../common/Form/Form';
+import Logo from '../../public/images/Logo.png';
 
-import './PasswordReset.scss';
+import '../Signup/Signup.scss';
 
 class PasswordReset extends Component {
   constructor() {
@@ -23,27 +24,32 @@ class PasswordReset extends Component {
 
   render() {
     return (
-      <div className="reset-container">
-        <Form paperStyle="reset-form">
-          <div className="reset-header">
-            <h1>Reset Password</h1>
-            <p>Enter in your email to request a password reset.</p>
+      <div className="auth-container">
+        <div className="auth-container__header">
+          <h2>Reset Password</h2>
+          <div className="auth-container__logo">
+            <img src={Logo} role="presentation" />
           </div>
-          <TextField
-            className="reset-input"
-            floatingLabelFocusStyle={{ color: red500 }}
-            underlineFocusStyle={{ borderColor: red500 }}
-            floatingLabelText="Email"
-            name="email"
-            onChange={this.handleOnChange}
-          />
-          <RaisedButton
-            label="Submit"
-            className="reset-btn"
-            backgroundColor={red500}
-            labelColor="#FFF"
-            type="submit"
-          />
+        </div>
+        <Form>
+          <div className="auth-container__textfield-container">
+            <p>Enter in your email to request a password reset.</p>
+            <TextField
+              className="auth-container__textfield"
+              floatingLabelFocusStyle={{ color: red500 }}
+              underlineFocusStyle={{ borderColor: red500 }}
+              floatingLabelText="Email"
+              name="email"
+              onChange={this.handleOnChange}
+            />
+            <RaisedButton
+              label="Submit"
+              className="auth-container__cta"
+              backgroundColor={red500}
+              labelColor="#FFF"
+              type="submit"
+            />
+          </div>
         </Form>
       </div>
     );
