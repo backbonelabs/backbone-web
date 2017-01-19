99f83f943f069d71ea5e7d0c7de97e38dedf0cca
Miguel Correa <miguelc1221@gmail.com>
2017-01-19 14:44:57 -0500

    add .env.local and mailgun apikey

--- server/config.js
+++ server/config.js
@@ -1,4 +1,4 @@
 export default {
   secretKey: process.env.JWT_SECRET_KEY || 'DevSecretKey',
-  mailgunKey: process.env.MAILGUN_SECRET_KEY || 'key-e973def1ab5567369e8ab125c19facd1',
+  mailgunKey: process.env.BL_MAILGUN_API,
 };
