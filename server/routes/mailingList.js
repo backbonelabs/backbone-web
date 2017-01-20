643763df0763efb06edf527def4a6e18f5d34245
Miguel Correa <miguelc1221@gmail.com>
2017-01-20 11:24:38 -0500

    change domain

--- server/routes/mailingList.js
+++ server/routes/mailingList.js
@@ -1,6 +1,10 @@
+import Debug from 'debug';
 import config from '../config';
 
-const domain = 'postkickstarterinterest@mail.gobackbone.com';
+const debug = Debug('routes:mailing-list');
+
+const domain = 'mail.gobackbone.com';
+const mailListDomain = 'postkickstarterinterest@mail.gobackbone.com';
 const mailgun = require('mailgun-js')({ apiKey: config.mailgunKey, domain });
 
 export default (req, res) => {
@@ -10,12 +14,11 @@ export default (req, res) => {
     return res.status(400).send({ error: 'Email must be filled out.' });
   }
 
-  mailgun.lists(domain).members().create({ address: email }, (err, body) => {
+  mailgun.lists(mailListDomain).members().create({ upsert: 'yes', address: email }, (err) => {
     if (err) {
-      console.log(err);
+      debug('Error adding email to list', req.body, err);
       return res.status(500).json({ error: 'An error has occurred, please try again later.' });
     }
-    console.log(body);
     return res.status(200).json({ success: 'Thanks for signing up.' });
   });
 };
