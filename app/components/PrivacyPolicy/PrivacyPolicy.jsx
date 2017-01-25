686c80ecf465e9a72b602175123fd1e54832b92c
Kevin Huang <huang.kev@gmail.com>
2017-01-24 16:17:11 -0800

    Add some simple styling

--- app/components/PrivacyPolicy/PrivacyPolicy.jsx
+++ app/components/PrivacyPolicy/PrivacyPolicy.jsx
@@ -1,8 +1,15 @@
+/* eslint-disable react/no-danger */
 import React from 'react';
+import Paper from 'material-ui/Paper';
 import privacyPolicy from '../../static/privacy-policy.html';
+import './PrivacyPolicy.scss';
 
 const PrivacyPolicy = () => (
-  <div dangerouslySetInnerHTML={{ __html: privacyPolicy }} /> // eslint-disable-line react/no-danger
+  <div className="outer-container">
+    <Paper zDepth={1}>
+      <div className="inner-container" dangerouslySetInnerHTML={{ __html: privacyPolicy }} />
+    </Paper>
+  </div>
 );
 
 export default PrivacyPolicy;
