9523c7d6b84f133111e2085fa0f8cfd0b8459451
Miguel Correa <miguelc1221@gmail.com>
2017-01-25 17:52:09 -0500

    add updateUser endpoint

--- server/routes/user/index.js
+++ server/routes/user/index.js
@@ -1,8 +1,10 @@
 import Router from 'express';
 import fetchUser from './fetchUser';
+import updateUser from './updateUser';
 
 const router = Router();
 
 router.get('/', fetchUser);
+router.post('/', updateUser);
 
 export default router;
