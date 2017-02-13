1552e0ccf5cd89b463a058dc9c5ec177e8bcb26d
Miguel Correa <miguelc1221@gmail.com>
2017-02-13 14:23:01 -0500

    upgrade to webpack 2.2.1

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,6 +1,5 @@
 const path = require('path');
 const webpack = require('webpack');
-const autoprefixer = require('autoprefixer');
 
 module.exports = {
   devtool: 'cheap-module-eval-source-map',
@@ -15,30 +14,31 @@ module.exports = {
   },
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoErrorsPlugin(),
+    new webpack.NoEmitOnErrorsPlugin(),
   ],
   resolve: {
-    extensions: ['', '.js', '.jsx'],
+    extensions: ['.js', '.jsx'],
   },
   module: {
-    loaders: [{
-      test: /\.jsx?$/,
-      exclude: /node_modules/,
-      loaders: ['babel'],
-    }, {
-      test: /\.(png|jpg|jpeg|gif|svg|woff|woff2|ttf|eot)$/,
-      loader: 'url-loader',
-    }, {
-      test: /\.scss$/,
-      exclude: /node_modules/,
-      loaders: ['style', 'css?sourceMap', 'postcss', 'sass?sourceMap'],
-    }],
-  },
-  postcss() {
-    return [
-      autoprefixer({
-        browsers: ['last 2 versions'],
-      }),
-    ];
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loaders: ['babel-loader'],
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif|svg|woff|woff2|ttf|eot)$/,
+        loader: 'url-loader',
+      },
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        loaders: [
+          'style-loader',
+          'css-loader',
+          'postcss-loader',
+          'sass-loader?sourceMap'],
+      },
+    ],
   },
 };
--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,7 +1,6 @@
 const path = require('path');
 const webpack = require('webpack');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const autoprefixer = require('autoprefixer');
 
 module.exports = {
   devtool: 'source-map',
@@ -13,43 +12,37 @@ module.exports = {
     filename: 'bundle.js',
   },
   plugins: [
-    new webpack.optimize.OccurenceOrderPlugin(),
     new webpack.DefinePlugin({
       'process.env': {
         NODE_ENV: JSON.stringify('production'),
       },
     }),
-    new webpack.optimize.DedupePlugin(),
     new webpack.optimize.UglifyJsPlugin({
-      compressor: {
-        warnings: false,
-      },
+      minimize: true, // minimize loaders
     }),
-    new ExtractTextPlugin('styles.css', {
-      allChunks: true,
-    }),
+    new ExtractTextPlugin({ filename: 'styles.css', allChunks: true, disable: false }),
   ],
   resolve: {
-    extensions: ['', '.js', '.jsx'],
+    extensions: ['.js', '.jsx'],
   },
   module: {
-    loaders: [{
-      test: /\.jsx?$/,
-      exclude: /node_modules/,
-      loaders: ['babel'],
-    }, {
-      test: /\.(png|jpg|jpeg|gif|svg|woff|woff2|ttf|eot)$/,
-      loader: 'url-loader',
-    }, {
-      test: /\.(scss|css)$/,
-      loader: ExtractTextPlugin.extract('style', 'css!postcss-loader!sass-loader'),
-    }],
-  },
-  postcss() {
-    return [
-      autoprefixer({
-        browsers: ['last 2 versions'],
-      }),
-    ];
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loaders: ['babel-loader'],
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif|svg|woff|woff2|ttf|eot)$/,
+        loader: 'url-loader',
+      },
+      {
+        test: /\.(scss|css)$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: 'css-loader!postcss-loader!sass-loader',
+        }),
+      },
+    ],
   },
 };
