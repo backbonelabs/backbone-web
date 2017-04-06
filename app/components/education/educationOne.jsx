3499d1286da6f13f8375d26861607ec8cf565fad
Miguel Correa <miguelc1221@gmail.com>
2017-04-06 17:28:31 -0400

    add test routes

---
+++ app/components/education/educationOne.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const EducationOne = () => (
+  <div>
+    <h1>This is the first educational content</h1>
+  </div>
+);
+
+export default EducationOne;
---
+++ app/components/education/educationTwo.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const EducationTwo = () => (
+  <div>
+    <h1>This is the second educational content</h1>
+  </div>
+);
+
+export default EducationTwo;
